6395f13342bb3f64b48d3bef1927800f474ca19c
Murphy Stroppa <murph33@gmail.com>
2015-06-08 08:23:09 -0700

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   root "welcome#index"
-  resources :recipes
-#  get "recipes/" => "recipes#index"
-#  get "recipes/:id" => "recipes#show"
-#  get "recipe/new" => "recipes#new"
-#  post "recipes" => "recipes#create"
-#
-#  get "recipe/:id" => "recipes#edit", as: :recipe
-#  patch "recipe/:id" => "recipes#update"
-#  delete "recipe/:id" => "recipes#delete"
+
+  get "recipes/" => "recipes#index"
+  get "recipes/:id" => "recipes#show", as: :recipe
+  get "recipe/new" => "recipes#new"
+  post "recipes" => "recipes#create"
+
+  get "recipes/:id/edit" => "recipes#edit", as: :edit_recipe
+  patch "recipes/:id" => "recipes#update"
+  delete "recipes/:id" => "recipes#delete"
 
 end
